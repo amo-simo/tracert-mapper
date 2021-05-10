617b413a4d944088dafde2a170e91c0a5f24897e
amo-simo <512.amosimo@gmail.com>
2021-05-10 15:24:20 +0100

    polished the code a bit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import ipinfo
-from utilities import traceroute, getLocation
+from utilities import traceroute, getLocation, mapDrawer
 import sys
 import gmplot
+
 try:
     t_address = sys.argv[1]
 except IndexError:
@@ -10,22 +11,22 @@ except IndexError:
 
 IPINFO_API_KEY= '' #your ipinfo api key
 handle = ipinfo.getHandler(IPINFO_API_KEY)
+
 #get the user's location to center the map
 u_loc = getLocation(None, handle)
+
 google_api_key = '' #your google maps api key (optional)
 gmap = gmplot.GoogleMapPlotter(u_loc[0], u_loc[1], 0,apikey=google_api_key)
 
-
+#get the traceroute addresses and initialize the coordinates list
 addresses = traceroute(t_address)
 locations = []
+
 for address in addresses:
     loc = getLocation(address, handle)
     if loc != 0:
         locations.append(loc)
     else: pass
-path = zip(*locations)
-gmap.plot(*path, edge_width=2, color='red')
-gmap.marker(u_loc[0], u_loc[1], color='red')
-gmap.marker(locations[-1][0], locations[-1][1], color='blue')
-gmap.draw('map.html')
+
+mapDrawer(gmap, locations, "map.html", u_loc)
 
